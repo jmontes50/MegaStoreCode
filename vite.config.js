efcc2da88120b6126a4aa1dadb5d3ba59c794f8f
Osmar Montesinos <jorge.montesinos@tecsup.edu.pe>
2024-02-29 21:09:08 -0500

    change config

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@headlessui/react': '@headlessui/react/dist/index.esm.js',
+      '@mui/material': '@mui/material/esm',
     },
   },
 })
